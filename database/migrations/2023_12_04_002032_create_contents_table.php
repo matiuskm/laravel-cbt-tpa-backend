43e4065b9d16054bfdbd69b0ac7efba0579d4724
matiuskm <matiuskm@gmail.com>
2023-12-09 23:18:07 +0700

    add contents & materials

--- database/migrations/2023_12_04_002032_create_contents_table.php
+++ database/migrations/2023_12_04_002032_create_contents_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('contents', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('section');
             $table->text('image_url')->nullable();
             $table->text('content');
             $table->timestamps();
